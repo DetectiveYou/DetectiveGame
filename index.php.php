aaae54c7ba51d74defac88f2baf0c886c4f7cf9e
Muse Numsin <muse.numsin@gmail.com>
2012-09-22 16:18:14 -0400

    Revert "first commit"
    This reverts commit 7088153362b297bfdbd7f938ef2e7e035720fb59.

--- index.php.php
+++
@@ -1,6 +0,0 @@
-Hello world! We're making detective game!!
-
-<?php
-$url = 'http://www.flickr.com/services/feeds/photos_public.gne';
-foreach(simplexml_load_file($url)->entry as $it) echo $it->content;
-?>
